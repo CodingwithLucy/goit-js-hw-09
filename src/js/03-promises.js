4f4354fd51734ca3003e14b6b7393add746fe1e7
CodingwithLucy <143715585+CodingwithLucy@users.noreply.github.com>
2024-02-17 23:16:28 +0100

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import Notiflix from 'notiflix';
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const delay = Number(event.target.elements.delay.value);
+  const step = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
+
+  const promises = Array.from({ length: amount }, (_, i) => {
+    return createPromise(i + 1, delay + i * step);
+  });
+
+  Promise.allSettled(promises).then(results => {
+    results.forEach((result, i) => {
+      if (result.status === 'fulfilled') {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${i + 1} in ${result.value.delay}ms`
+        );
+      } else {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${i + 1} in ${result.reason.delay}ms`
+        );
+      }
+    });
+  });
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
